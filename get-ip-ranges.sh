f7b085d46d618e80c638420ab88c0f7a146a038b
Oliver Arafat <oliver.arafat@outlook.com>
2017-02-27 13:53:23 +0100

    printing output to stdout

--- get-ip-ranges.sh
+++ get-ip-ranges.sh
@@ -1,6 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 FIRSTLONGPART="https://download.microsoft.com/download/0/1/8/018E208D-54F8-44CD-AA26-CD7BC9524A8C/PublicIPs_"
 INITIALURL="http://www.microsoft.com/EN-US/DOWNLOAD/confirmation.aspx?id=41653"
-OUT="$(curl -s $INITIALURL | grep -o -P '(?<='$FIRSTLONGPART').*(?=.xml")'|tail -1)"
+OUT="$(curl -s $INITIALURL | grep -o -P '(?<='$FIRSTLONGPART').*(?=.xml")' | tail -1)"
 wget -nv $FIRSTLONGPART$OUT".xml"
-mv "PublicIPs_"$OUT".xml" "azure_ip_ranges.xml"
+cat "PublicIPs_"$OUT".xml"
+rm "PublicIPs_"$OUT".xml"
